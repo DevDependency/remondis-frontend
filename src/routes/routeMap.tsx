b1b1320a9e1a5d1345466a26a23be47c364f0098
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-12-02 22:52:12 +0100

    refactor(app): comment loaders in routes

--- src/routes/routeMap.tsx
+++ src/routes/routeMap.tsx
@@ -7,25 +7,25 @@ export const routeMap : RouteObject[] = [
   {
     path: Locations.ROOT,
     element: <><Main /><Outlet/> </>,
-    loader: ()=>{},
+    //loader: ()=>{},
     children: []
   },
   {
     path: Locations.SIGNUP,
-    element: <> <SignUp /> <Outlet/> </>,
-    loader: ()=>{},
+    element: <SignUp />,
+    //loader: ()=>{},
     children: []
   },
   {
     path: Locations.LOGIN,
     element: <Login />,
-    loader: ()=>{},
+    //loader: ()=>{},
     children: []
   },
   {
     path: Locations.PAGE_NOT_FOUND,
     element: <PageNotFound />,
-    loader: ()=>{},
+    //loader: ()=>{},
     children: []
   }
 ]
