66fd0a6fb2e6eec392c9857764b3ba7048b91170
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-12-02 13:25:54 +0100

    feat(app): add template pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Main } from "./Main";
+import { PageNotFound } from "./PageNotFound";
+import { SignUp } from "./SingUp";
+import { Login } from "./Login";
+
+
+export {
+  Main,
+  PageNotFound,
+  SignUp,
+  Login
+}
