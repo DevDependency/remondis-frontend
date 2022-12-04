4a9d63d5f2e32da77e4870f563ba79809c6571e8
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-12-04 23:51:15 +0100

    chore(case): add in Main component create new case functionality for test

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,39 @@
-export const Main : React.FC = () => {
-  return (
-    <>Main page</>
-  )
-}
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { createCase } from "../store/slices/case";
+import { useAppDispatch, useAppSelector } from "../utils/hooks/useStore";
+
+
+export const Main: React.FC = () => {
+
+  // test create new case
+  const dispatch = useAppDispatch();
+  const createdCaseId = useAppSelector((state) => state.caseSlice.createdCaseId);
+  const navigate = useNavigate();
+
+  useEffect( () => {
+    if(createdCaseId !== 0 ) {
+      console.log(createdCaseId)
+      navigate(`/cases/${createdCaseId}`)
+    }
+  }, [createdCaseId])
+
+  const addCase = () => {
+    dispatch(
+      createCase({
+        client_first_name: "first name",
+        client_last_name: "last name",
+        client_phone: "phone",
+        client_email: "email@email.com",
+        address: "adress",
+        manager_id: 1,
+      })
+    );
+  };
+
+
+  return (<>
+  Main page
+  <button onClick={addCase}>New Case</button>
+  </>);
+};
