680699450b6eb1eb632ba61f8a8fc5a870307982
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-12-05 15:43:16 +0100

    feat(app): add index file for containers components

---
+++ src/components/containers/index.tsx
@@ -0,0 +1,13 @@
+import { NavBar } from "./NavBar";
+import { TabBarManager } from "./TabBarManager";
+import { TabBarInspector } from "./TabBarInspector";
+import { TabBarCase } from "./TabBarCase";
+import { ButtonContainer } from "./ButtonContainer";
+
+export {
+  NavBar,
+  TabBarManager,
+  TabBarInspector,
+  TabBarCase,
+  ButtonContainer,
+}
