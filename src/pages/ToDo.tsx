70fc41223deca44b362865d0b738bb3cc5b78b64
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-12-05 17:57:27 +0100

    feat(case): render todo and cases

--- src/pages/ToDo.tsx
+++ src/pages/ToDo.tsx
@@ -1,5 +1,29 @@
-export const ToDo : React.FC = () => {
+import { useEffect } from "react";
+import { TabBarManager } from "../components/containers/TabBarManager";
+import { useAppSelector, useAppDispatch } from "../utils/hooks/useStore";
+import { getCasesToDo } from "../store/slices/case";
+
+export const ToDo: React.FC = () => {
+  const tasks = useAppSelector((state) => state.caseSlice.casesToDo);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    // TODO put real user ID
+    dispatch(getCasesToDo(1));
+  }, []);
   return (
-    <>Todo</>
-  )
-}
+    <>
+      <TabBarManager />
+      {tasks &&
+        tasks
+        .map((item: any, index) => (
+          <div key={index}>
+            time={new Date(item.created_at).toLocaleDateString("en-Us")}
+            address={item.address}
+            link={item.id}
+            isTodo={false}
+          </div>
+        ))}
+    </>
+  );
+};
