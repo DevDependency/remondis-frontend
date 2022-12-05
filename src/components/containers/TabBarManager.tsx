0a9cbd1076f74696bdbfeb244b93c5ed3742c9da
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-12-05 17:54:45 +0100

    feat(app): add tab bars inspector and manager

--- src/components/containers/TabBarManager.tsx
+++ src/components/containers/TabBarManager.tsx
@@ -1,5 +1,18 @@
-export const TabBarManager : React.FC = () => {
+import { Link } from "react-router-dom";
+import {TAB_BAR_MANAGER} from "../../utils/constants"
+
+
+export const TabBarManager: React.FC = () => {
+  
   return (
-    <>Tab bar manager</>
-  )
-}
+    <>
+      {TAB_BAR_MANAGER.map((tab) => (
+        <Link to={tab.path} key={tab.id}>
+          <div id={tab.id}>
+            {tab.name}
+          </div>
+        </Link>
+      ))}
+    </>
+  );
+};
