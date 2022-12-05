a1b3a58862f1e74e613e64af3f6eae7a82bec74c
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-12-05 21:42:30 +0100

    style(app): add space inn NavBar

--- src/components/containers/NavBar.tsx
+++ src/components/containers/NavBar.tsx
@@ -1,6 +1,9 @@
 import { useAppSelector } from "../../utils/hooks/useStore";
 import { Link } from "react-router-dom";
+
+
 export const NavBar: React.FC = () => {
+  
   const insideCase = useAppSelector((state) => state.generalSlice.insideCase);
 
   return (
