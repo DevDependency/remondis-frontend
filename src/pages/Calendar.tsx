29cbc714ee91684d258766a99d21240f21d3e1db
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-12-05 18:45:30 +0100

    feat(app): add empty components map and calendar

---
+++ src/pages/Calendar.tsx
@@ -0,0 +1,10 @@
+import { TabBarInspector } from "../components/containers/";
+
+export const Calendar: React.FC = () => {
+  return (
+    <>
+      <TabBarInspector />
+      Calendar
+    </>
+  );
+};
---
+++ src/pages/Map.tsx
@@ -0,0 +1,11 @@
+import { TabBarInspector } from '../components/containers/';
+
+
+export const Map : React.FC = () => {
+  return (
+    <>
+    <TabBarInspector />
+    Map
+    </>
+  )
+}
