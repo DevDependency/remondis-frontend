64cf3269336185b6e2f231d5d235a4cb6ba75ada
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-12-07 14:36:27 +0100

    feat(route): change edit routes

--- src/components/containers/ButtonContainer.tsx
+++ src/components/containers/ButtonContainer.tsx
@@ -1,9 +1,15 @@
 import { ButtonContainerStyled, ButtonSmallStyled, ButtonStyled } from "../../styles/style";
+import { useNavigate, RelativeRoutingType } from 'react-router-dom';
+import { replace } from "formik";
 
 export const ButtonContainer: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <ButtonContainerStyled>
-      <ButtonSmallStyled>Edit</ButtonSmallStyled>
+      <ButtonSmallStyled onClick={() => {
+              navigate('edit', {replace: true, relative: "path" });
+            }}>Edit</ButtonSmallStyled>
       <ButtonSmallStyled color={"red"}>Submit</ButtonSmallStyled>
     </ButtonContainerStyled>
   );
