a039ec879b76e03a011c1d7cab944b73048336db
Kravchuk Andrey <aksnt13@gmail.com>
2022-12-07 09:58:10 +0100

    feat(case): case details edit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -73,3 +73,21 @@ export const TYPE_OF_PROPERTIES = [
     name: "Backyard"
   },
 ]
+
+export const FLOOR = [
+  { name: "1", value: 1 },
+  { name: "2", value: 2 },
+  { name: "3", value: 3 },
+  { name: "4", value: 4 },
+  { name: "5", value: 5 },
+  { name: "6", value: 6 },
+]
+
+export const NUMBER_OF_ROOMS = [
+  { name: "1", value: 1 },
+  { name: "2", value: 2 },
+  { name: "3", value: 3 },
+  { name: "4", value: 4 },
+  { name: "5", value: 5 },
+  { name: "6", value: 6 },
+];
