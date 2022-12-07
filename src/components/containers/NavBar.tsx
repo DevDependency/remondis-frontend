decfe25f1ec2a3da9ee9489403fe81d682d8983a
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-12-07 22:48:42 +0100

    feat(app): add to NavBar back link inside case

--- src/components/containers/NavBar.tsx
+++ src/components/containers/NavBar.tsx
@@ -1,10 +1,10 @@
 import { useAppSelector, useAppDispatch } from "../../utils/hooks/useStore";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 import { LogoStyled, IconStyled, NavBarStyled } from "../../styles/style";
 import account from "../../assets/-navbar-iconAccount.svg";
 import iconStatusFree from "../../assets/-navbar-iconStatusFree.svg";
 import iconStatusBusy from "../../assets/-navbar-iconStatusBusy.svg";
-import back from "../../assets/-navbar-iconBack.svg";
+import iconBack from "../../assets/-navbar-iconBack.svg";
 import logo from "../../assets/logoREMONDISx112.png";
 import { setIsPopupVisible } from "../../store/slices/general";
 import { PopUpConfirm } from "../PopUpConfirm";
@@ -12,14 +12,21 @@ import { setIsInspectorActive, updateUser } from "../../store/slices/user";
 
 
 export const NavBar: React.FC = () => {
-  const insideCase = useAppSelector((state) => state.generalSlice.insideCase);
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const insideCase = useAppSelector((state) => state.generalSlice.insideCase);
   const isPopupVisible = useAppSelector(state => state.generalSlice.isPopupVisible);
   const userId = useAppSelector(state => state.userSlice.userId);
   const isInspectorActive = useAppSelector(state => state.userSlice.isInspectorActive);
+  
   const handler = () => {
     dispatch(setIsPopupVisible(true));
   }
+  
+  const setPopUpUnVisible = () => {
+    dispatch(setIsPopupVisible(false));    
+  }
+
   const setInspectorStatus = () => {
     dispatch(updateUser({
       userId: userId, 
@@ -29,17 +36,14 @@ export const NavBar: React.FC = () => {
     dispatch(setIsPopupVisible(false));
     dispatch(setIsInspectorActive());
   }
-  const setPopUpUnVisible = () => {
-    dispatch(setIsPopupVisible(false));    
-  }
   
   return (
     <>
       <NavBarStyled>
-        <LogoStyled src={logo} alt=""></LogoStyled>
+        {insideCase ? <IconStyled src={iconBack} onClick={ () => navigate("/")} /> : <LogoStyled src={logo} alt=""></LogoStyled>}
         <IconStyled src={isInspectorActive? iconStatusBusy : iconStatusFree} onClick={handler}/>
         <IconStyled src={account} />
-        {insideCase && <Link to="/"></Link>}
+        
       </NavBarStyled>
       {
       isPopupVisible && 
