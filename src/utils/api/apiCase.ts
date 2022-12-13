bd2b8dafbfca49fd28bb8417e2575b69faa4382c
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-12-13 11:26:01 +0100

    feat(case): add case changed flag as trigger to getting data for update

--- src/utils/api/apiCase.ts
+++ src/utils/api/apiCase.ts
@@ -51,7 +51,7 @@ export const apiPatchCaseById = async (
   caseId: number,
   changedData: Case
 ) => {
-  const { Appointment, Inspector, ...body } = changedData;
+  const { Appointment, Inspector, State, ...body } = changedData;
   console.log(changedData)
   try {
     const res = await server.patch(`cases/${caseId}`, body);
